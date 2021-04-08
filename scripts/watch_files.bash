c04cbb8f5825b55c51a011b3887b4b2597df12a4
Aaron Lelouvier <aaron.lelouvier@protonmail.com>
2021-04-08 20:38:00 +0200

    Added helper script to rebuilt site on file change.

---
+++ scripts/watch_files.bash
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+files=(base.html de.yaml it.yaml en.yaml)
+len=${#files[@]}
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+while true; do
+    hashes=()
+    for (( n=0; n<len; n++ )); do
+        hash=$(md5sum ${files[$n]})
+        hash=($hash)
+        hashes=(${hashes[@]} "$hash")
+    done
+    sleep 1
+    for (( n=0; n<len; n++ )); do
+        hash=$(md5sum ${files[$n]})
+        hash=($hash)
+        if [ "$hash" != "${hashes[$n]}" ]; then
+            source "$DIR/compile_css.bash"
+            python "$DIR/generate_pages.py" "--preview"
+        fi
+    done
+done
