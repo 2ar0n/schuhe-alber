c0a9039cafe849759f0224d4ee7b152a1c1c28b4
Aaron Lelouvier <aaron.lelouvier@protonmail.com>
2021-04-08 18:34:39 +0200

    Fixed tailwindcss dev env.

--- scripts/compile_css.bash
+++ scripts/compile_css.bash
@@ -6,4 +6,4 @@ elif [[ $(command -v docker) ]]; then
     COMMAND="docker"
 fi
 
-$COMMAND run -it -v "$PWD":/tmp/site localhost/tailwindcss npx tailwindcss-cli@latest build -o styles.css
+$COMMAND run -it -v "$PWD":/tmp/site/build localhost/tailwindcss npx tailwind build build/tailwind.css -c build/tailwind.config.js -o build/styles.css
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ module.exports = {
   purge: {
       enabled: true,
       content: [
-        'base.html',
+        'build/base.html', // see scripts/compile_css.bash
       ],
   },
   darkMode: false, // or 'media' or 'class'
