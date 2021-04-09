9223bea592c61846fb10a09a92458c8aa2949315
Aaron Lelouvier <aaron.lelouvier@protonmail.com>
2021-04-09 20:15:13 +0200

    Serve site locally using python instead.

---
+++ scripts/serve.py
@@ -0,0 +1,23 @@
+#!/bin/python
+
+import http.server
+import socketserver
+import argparse
+
+
+parser = argparse.ArgumentParser(description="Generate website pages.")
+parser.add_argument("--port", type=int, default=8080, help="port to serve")
+parser.add_argument("--dir", default=None, help="dir to serve")
+
+args = parser.parse_args()
+
+class Handler(http.server.SimpleHTTPRequestHandler):
+    def __init__(self, *arg, **kwargs):
+        super().__init__(*arg, directory=args.dir, **kwargs)
+
+with socketserver.TCPServer(("", args.port), Handler) as httpd:
+    try:
+        print("Serving at port", args.port)
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
--- scripts/serve_in_container.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if [[ $(command -v podman) ]]; then
-    COMMAND="sudo podman"
-elif [[ $(command -v docker) ]]; then
-    COMMAND="docker"
-fi
-
-$COMMAND run -dit --name apache-httpd -p 8080:80 -v "$PWD/_site":/usr/local/apache2/htdocs/ httpd:2.4
